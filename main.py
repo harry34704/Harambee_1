467534d733de743530cdfab185e1f82739470c30
Harry Munyai <munyaiharry3@gmail.com>
2025-03-04 21:21:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import os
+from flask import Flask
 from app import app
+from flask_sqlalchemy import SQLAlchemy
+# app = Flask(__name__) # Remove this line
+# Use the Render PostgreSQL database URL from environment variables
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     # ALWAYS serve the app on port 5000

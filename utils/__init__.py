64415a9ded53dc48251ef449b4e091a05b5eab3d
Harry Munyai <munyaiharry3@gmail.com>
2025-03-04 21:31:08 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,22 @@
+# app/__init__.py
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        # import your routes here
+        from . import routes
+
+    return app
 
 # This file makes the utils directory a Python package
